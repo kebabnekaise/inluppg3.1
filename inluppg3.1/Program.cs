a2c7181d2980489810b1280cf3c155ed2dc509df
emsi06001 <emsi06001@E-PW0234N3>
2023-10-04 12:20:34 +0200

    Add project files.

---
+++ inluppg3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace inluppg3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ans = int.Parse(Console.ReadLine());
+            if (ans <= 19 && ans >= 16)
+            {
+                Console.WriteLine("Du får delta");
+            }
+            else if (ans < 16)
+            {
+                Console.WriteLine("Du är för ung");
+            }
+            else
+            {
+                Console.WriteLine("Du är för gammal");
+            }
+        }
+    }
+}
